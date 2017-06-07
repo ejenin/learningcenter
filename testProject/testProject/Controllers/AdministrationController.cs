ce4fd1829caeb6f8ccec0f1ab8338bbe6a022d52
Jekus <zukaatoku@gmail.com>
2017-06-07 18:00:09 +0400

    big commit

--- testProject/testProject/Controllers/AdministrationController.cs
+++ testProject/testProject/Controllers/AdministrationController.cs
@@ -4,17 +4,29 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace testProject.Controllers
-{
-    public class AdministrationController : Controller
-    {
+namespace testProject.Controllers {
+    public class AdministrationController : Controller {
         //
         // GET: /Administration/
 
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
 
+        public ActionResult CitiesConfig() {
+            return View();
+        }
+
+        public ActionResult Reports() {
+            return View();
+        }
+
+        public ActionResult Planning() {
+            return View();
+        }
+
+        public ActionResult Progress() {
+            return View();
+        }
     }
 }
